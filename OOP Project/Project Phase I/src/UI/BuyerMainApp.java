d98139afa21caa6158e5675c1abea7c0a30f97b8
Maroibo <Marwan2262004@gmail.com>
2022-04-18 23:28:31 +0300

    Made the buyer main panel

---
+++ OOP Project/Project Phase I/src/UI/BuyerMainApp.java
@@ -0,0 +1,24 @@
+package UI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BuyerMainApp extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+    	String viewFileName = "buyerMainPane.fxml";
+    	String windowTite = "Buyer UI Window";
+        Parent root = FXMLLoader.load(getClass().getResource(viewFileName));
+        stage.setTitle(windowTite);
+        stage.setScene(new Scene(root, 700, 450));
+        stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
